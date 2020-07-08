cab3f55b5ccf3e414853677688db641ee0ddab8e
Norawit Nangsue <norawitn@hotmail.com>
2020-07-08 11:18:49 +0700

    update

--- roboteq_tcp_driver/src/roboteq_tcp_driver/driver_impl.py
+++ roboteq_tcp_driver/src/roboteq_tcp_driver/driver_impl.py
@@ -279,6 +279,7 @@ class DriverImplementation(object):
         """
         Update Error Status
         """
+        rospy.loginfo('/fault/driver was set to %s', description)
         rospy.set_param('/fault/driver', description)
         self.error = description
     # protected region user additional functions end #
